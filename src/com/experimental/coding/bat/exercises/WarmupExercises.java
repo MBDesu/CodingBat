705cbe2a821e2faf4b648eac754e22cc679b9ac0
Jordyn <>
2018-06-06 14:13:26 -0400

    Finished Warmup-2

--- src/com/experimental/coding/bat/exercises/WarmupExercises.java
+++ src/com/experimental/coding/bat/exercises/WarmupExercises.java
@@ -173,5 +173,134 @@ public class WarmupExercises {
 		}
 		return result;
 	} // END OF Warmup-1
+	
+	public String stringTimes(String str, int n) {
+		if(n == 0 || "".equals(str)) return "";
+		if(n == 1) return str;
+		return str + stringTimes(str, --n);
+	}
+	
+	public String frontTimes(String str, int n) {
+		if(n == 0 || "".equals(str)) return "";
+		String front = str.length() < 3 ? str : str.substring(0, 3);
+		if(n == 1) return front;
+		return front + frontTimes(str, --n);
+	}
+	
+	public int countXX(String str) {
+		if(str.length() < 2) return 0;
+		return "xx".equals(str.substring(0, 2)) ? 1 + countXX(str.substring(1)) : countXX(str.substring(1));
+	}
+	
+	public boolean doubleX(String str) {
+		int pos = str.indexOf('x');
+		if(pos+1 >= str.length()) return false;
+		return pos >= 0 ? str.charAt(pos + 1) == 'x' : false;
+	}
+	
+	public String stringBits(String str) {
+		String result = "";
+		for(int i = 0; i < str.length(); i+=2) {
+			result += str.substring(i, i+1);
+		}
+		return result;
+	}
+	
+	public String stringSplosion(String str) {
+		String result = "";
+		for(int i = 0; i < str.length(); i++) {
+			result += str.substring(0, i+1);
+		}
+		return result;
+	}
+	
+	public int last2(String str) {
+		int len = str.length();
+		if(len <= 2) return 0;
+		int count = 0;
+		String end = str.substring(len - 2);
+		for(int i = 0; i < len - 2; i++) {
+			if(str.substring(i, i + 2).equals(end)) count++;
+		}
+		return count;
+	}
+	
+	public int arrayCount9(int[] nums) {
+		int count = 0;
+		for(Integer i : nums) {
+			if(i == 9) count++;
+		}
+		return count;
+	}
+	
+	public boolean arrayFront9(int[] nums) {
+		int bound = nums.length < 4 ? nums.length : 4;
+		for(int i = 0; i < bound; i++) {
+			if(nums[i] == 9) return true;
+		}
+		return false;
+	}
+	
+	public boolean array123(int[] nums) {
+		for(int i = 0; i < nums.length - 2; i++) {
+			if((nums[i] == 1)) {
+				if(nums[i + 1] == 2 && nums[i + 2] == 3) return true;
+			}
+		}
+		return false;
+	}
+	
+	public int stringMatch(String a, String b) {
+		int count = 0;
+		int len = Math.min(a.length(), b.length());
+		for(int i = 0; i < len-1; i++) {
+			if(a.substring(i, i+2).equals(b.substring(i, i+2))) count++;
+		}
+		return count;
+	}
+	
+	public String stringX(String str) {
+		String result = "";
+		for(int i = 0; i < str.length(); i++) {
+			if(!(i > 0 && i < str.length() - 1 && str.substring(i, i+1).equals("x"))) result += str.substring(i, i+1);
+		}
+		return result;
+	}
+	
+	public String altPairs(String str) {
+		String result = "";
+		for(int i = 0; i < str.length(); i+=4) { 
+			int end = i + 2;
+			if(end > str.length()) end = str.length();
+			result += str.substring(i, end);
+		}
+		return result;
+	}
+	
+	public String stringYak(String str) {
+		return str.replaceAll("([yY]{1}.{1}[kK]{1})", "");
+	}
+	
+	public int array667(int[] nums) {
+		int count = 0;
+		for(int i = 0; i < nums.length - 1; i++) {
+			if(nums[i] == 6 && (nums[i+1] == 6 || nums[i+1] == 7)) count++;
+		}
+		return count;
+	}
+	
+	public boolean noTriples(int[] nums) {
+		for(int i = 1; i < nums.length - 1; i++) {
+			if(nums[i] == nums[i-1] && nums[i] == nums[i+1]) return false;
+		}
+		return true;
+	}
+	
+	public boolean has271(int[] nums) {
+		for(int i = 1; i < nums.length - 1; i++) {
+			if((nums[i-1] == nums[i] - 5) && Math.abs((nums[i-1] - 1) - nums[i+1]) <= 2) return true;
+		}
+		return false;
+	} // END OF Warmup-2
 
 }
