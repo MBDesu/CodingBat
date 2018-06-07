6367000dc8d4c5ff0c294e87141ae471488376ca
Jordyn <>
2018-06-07 09:43:16 -0400

    Just kidding, found a boolean inversion to fix

--- src/com/experimental/coding/bat/exercises/FunctionalExercises.java
+++ src/com/experimental/coding/bat/exercises/FunctionalExercises.java
@@ -98,7 +98,7 @@ public class FunctionalExercises {
 	public List<Integer> two2(List<Integer> nums) {
 		return nums.stream()
 				.map(n -> n * 2)
-				.filter(n -> !(n % 10 == 2))
+				.filter(n -> n % 10 != 2)
 				.collect(Collectors.toList());
 	}
 
