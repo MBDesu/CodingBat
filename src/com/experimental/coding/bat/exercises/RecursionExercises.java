8bedd79209dc33813056560f32a371bc2da06d83
Jordyn <>
2018-06-20 07:50:31 -0400

    Did some more backtracking problems

--- src/com/experimental/coding/bat/exercises/RecursionExercises.java
+++ src/com/experimental/coding/bat/exercises/RecursionExercises.java
@@ -383,8 +383,55 @@ public class RecursionExercises {
 	 */
 	public boolean groupSum(int start, int[] nums, int target) {
 		if(start >= nums.length) return (target == 0);
-		if(groupSum(start + 1, nums, target - nums[start])) return true; // backtracking is hard
-		if(groupSum(start + 1, nums, target)) return true; // really hard
+		if(groupSum(start + 1, nums, target - nums[start])) return true;
+		if(groupSum(start + 1, nums, target)) return true;
+		return false;
+	}
+	
+	/*
+	 * Given an array of ints, is it possible to choose a group of some of the ints,
+	 * beginning at the start index, such that the group sums to the given target?
+	 * However, with the additional constraint that all 6's must be chosen. (No
+	 * loops needed.)
+	 */
+	public boolean groupSum6(int start, int[] nums, int target) {
+		if(start >= nums.length) return (target == 0);
+		if(nums[start] == 6) return groupSum6(start + 1, nums, target - 6);
+		if(groupSum6(start + 1, nums, target - nums[start])) return true;
+		if(groupSum6(start + 1, nums, target)) return true;
+		return false;
+	}
+
+	/*
+	 * Given an array of ints, is it possible to choose a group of some of the ints,
+	 * such that the group sums to the given target with this additional constraint:
+	 * If a value in the array is chosen to be in the group, the value immediately
+	 * following it in the array must not be chosen. (No loops needed.)
+	 */
+	public boolean groupNoAdj(int start, int[] nums, int target) {
+		if(start >= nums.length) return (target == 0);
+		if(groupNoAdj(start + 2, nums, target - nums[start])) return true;
+		if(groupNoAdj(start + 1, nums, target)) return true;
+		return false;
+	}
+
+	/*
+	 * Given an array of ints, is it possible to choose a group of some of the ints,
+	 * such that the group sums to the given target with these additional
+	 * constraints: all multiples of 5 in the array must be included in the group.
+	 * If the value immediately following a multiple of 5 is 1, it must not be
+	 * chosen. (No loops needed.)
+	 */
+	public boolean groupSum5(int start, int[] nums, int target) {
+		if(start >= nums.length) return (target == 0);
+		if(nums[start] % 5 == 0) {
+			if(start <= nums.length - 2 && nums[start + 1] == 1) {
+				return groupSum5(start + 2, nums, target - nums[start]);
+			}
+			return groupSum5(start + 1, nums, target - nums[start]);
+		}
+		if(groupSum5(start + 1, nums, target - nums[start])) return true;
+		if(groupSum5(start + 1, nums, target)) return true;
 		return false;
 	}
 	
